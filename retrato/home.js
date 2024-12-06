2cd8deb5fdf375503f0039eecf96840d228f8447
sofifi11 <sofia2911torres@gmail.com>
2024-12-06 11:16:10 -0300

    Add files via upload

---
+++ retrato/home.js
@@ -0,0 +1,35 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+    let i;
+    if (document.querySelector(".carousel")) {
+        let slideshowPage = document.querySelector(".carousel");
+        if (slideshowPage.classList.contains("carousel")) {
+            let slides = slideshowPage.querySelectorAll(".mySlides");
+            if (n > slides.length) {
+            slideIndex = 1
+            };
+            if (n < 1) {
+            slideIndex = slides.length
+            };
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        };
+        slides[slideIndex-1].style.display = "block";
+        }
+    }
+}
+
+
+setInterval(function() {
+  plusSlides(1)
+}, 6000);
